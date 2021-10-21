abafc3a7986fd3dbde19fcdaa47ff7e2d5c0fe8a
Guillaume LASNE <guillaume.lasne@netseenergy.fr>
2021-10-21 11:16:06 +0200

    Adding scaffolding for game parsing

--- src/main/java/bowling/game/BowlingGame.java
+++ src/main/java/bowling/game/BowlingGame.java
@@ -1,16 +1,31 @@
 package bowling.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-  private BowlingGame() {
+  private final List<Integer> knockedPins;
 
+  private BowlingGame(List<Integer> knockedPins) {
+    this.knockedPins = knockedPins;
   }
 
   public static BowlingGame of(String game) {
-    return new BowlingGame();
+    List<Integer> knockedPins = new ArrayList<>();
+    for (char c:game.toCharArray()) {
+      if(c == '-') {
+        knockedPins.add(0);
+      }
+    }
+    return new BowlingGame(knockedPins);
   }
 
   public int score() {
     return 0;
+  }
+
+  public List<Integer> getKnockedPins() {
+    return knockedPins;
   }
 }
--- src/test/java/bowling/game/BowlingGameTest.java
+++ src/test/java/bowling/game/BowlingGameTest.java
@@ -2,14 +2,26 @@ package bowling.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 class BowlingGameTest {
   @Test
-  void the_score_of_a_game_with_no_knocked_pins_is_zero() {
-    String game = "--|--|--|--|--|--|--|--|--|--||";
-    BowlingGame bowlingGame = BowlingGame.of(game);
+  void the_score_of_a_game_with_only_miss_pins_is_zero() {
+    String gameWithOnlyMiss = "--|--|--|--|--|--|--|--|--|--||";
+    BowlingGame bowlingGame = BowlingGame.of(gameWithOnlyMiss);
     int score = bowlingGame.score();
     assertThat(score).isZero();
+  }
+
+  @Test
+  void the_knocked_pins_of_a_game_with_only_miss_is_twenty_miss() {
+    String gameWithOnlyMiss = "--|--|--|--|--|--|--|--|--|--||";
+    BowlingGame bowlingGame = BowlingGame.of(gameWithOnlyMiss);
+    List<Integer> twentyMiss = IntStream.iterate(0, x -> 0).limit(20).boxed().toList();
+
+    assertThat(bowlingGame.getKnockedPins()).isEqualTo(twentyMiss);
   }
 }
